23dab13f1bf67f931bab37c9029b7c16688355f4
Han4578 <99018603+Han4578@users.noreply.github.com>
2026-07-01 00:02:11 +0800

    excluded files in python folder from sw

--- update_sw_list.py
+++ update_sw_list.py
@@ -5,6 +5,6 @@ import pyperclip
 files = ["/"]
 
 for e in ["html", "css", "js", "json", "png", "webp"]:
-    files.extend(t.replace("\\", "/") for t in glob.glob(f"**/*.{e}", recursive=True))
+    files.extend(t.replace("\\", "/") for t in glob.glob(f"**/*.{e}", recursive=True) if "python" not in t)
 
 pyperclip.copy(str(files))
