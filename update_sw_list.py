969f351cb18d6a4d8aa05a56ed8c28b9d228e3b3
Han4578 <99018603+Han4578@users.noreply.github.com>
2026-07-01 00:12:43 +0800

    added more image types to cache

--- update_sw_list.py
+++ update_sw_list.py
@@ -4,7 +4,7 @@ import pyperclip
 
 files = ["/"]
 
-for e in ["html", "css", "js", "json", "png", "webp"]:
+for e in ["html", "css", "js", "json", "png", "webp", "svg", "jpg", "jpeg"]:
     files.extend(t.replace("\\", "/") for t in glob.glob(f"**/*.{e}", recursive=True) if "python" not in t)
 
 pyperclip.copy(str(files))
