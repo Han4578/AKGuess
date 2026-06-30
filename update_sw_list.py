1f2df7eaac479fa0a4f73e73a96000115f164dee
Han4578 <99018603+Han4578@users.noreply.github.com>
2026-06-30 23:40:58 +0800

    updated to use forward slash for file paths

--- update_sw_list.py
+++ update_sw_list.py
@@ -5,6 +5,6 @@ import pyperclip
 files = ["/"]
 
 for e in ["html", "css", "js", "json", "png", "webp"]:
-    files.extend(glob.glob(f"**/*.{e}", recursive=True))
+    files.extend(t.replace("\\", "/") for t in glob.glob(f"**/*.{e}", recursive=True))
 
 pyperclip.copy(str(files))
